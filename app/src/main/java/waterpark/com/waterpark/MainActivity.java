cc9ad234ce63a83a5fbf1fcdadc5b674bdab95c7
vishalshubham <vishal.shubham@gmail.com>
2015-10-24 10:47:29 -0400

    first commit

---
+++ app/src/main/java/waterpark/com/waterpark/MainActivity.java
@@ -0,0 +1,67 @@
+package waterpark.com.waterpark;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final GoogleMap mMap = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        MarkerOptions mOptions = new MarkerOptions();
+        mOptions.title("My place");
+        LatLng coordinate = new LatLng(43.4619415152536, -80.5222363389286);
+        mOptions.position(coordinate);
+
+        CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(coordinate, 12);
+        mMap.animateCamera(yourLocation);
+        mMap.addMarker(mOptions);
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                MarkerOptions newMarker = new MarkerOptions();
+                newMarker.position(latLng);
+                newMarker.title("My Marker");
+                mMap.addMarker(newMarker);
+            }
+        });
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+}
