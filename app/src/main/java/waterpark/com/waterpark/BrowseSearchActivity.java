d6bab4cc552713dd14d4fde72730e043a03d351f
vishalshubham <vishal.shubham@gmail.com>
2015-10-25 12:37:57 -0400

    UI changes and some extra logic changes

--- app/src/main/java/waterpark/com/waterpark/BrowseSearchActivity.java
+++ app/src/main/java/waterpark/com/waterpark/BrowseSearchActivity.java
@@ -2,6 +2,8 @@ package waterpark.com.waterpark;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +12,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
 
 public class BrowseSearchActivity extends Activity {
 
@@ -23,6 +29,7 @@ public class BrowseSearchActivity extends Activity {
         final RadioButton motorcycleButton = (RadioButton)findViewById(R.id.id_rad_motorcycle);
         final RadioButton carButton = (RadioButton)findViewById(R.id.id_rad_car);
 
+        Button buttonSearchLocation = (Button)findViewById(R.id.id_button_search);
         Button buttonCurrentLocation = (Button)findViewById(R.id.id_button_current_location);
         buttonCurrentLocation.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,10 +44,48 @@ public class BrowseSearchActivity extends Activity {
 
                 Log.d(DEBUGTAG, "Vehicle: " + decidedVehicle);
                 Intent i = new Intent(BrowseSearchActivity.this, MainActivity.class);
+                i.putExtra("searchType", 2);
                 i.putExtra("vehicle", decidedVehicle);
                 startActivity(i);
             }
         });
+
+        buttonSearchLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Geocoder geocoder = new Geocoder(BrowseSearchActivity.this);
+                TextView textView = (TextView)findViewById(R.id.text_search_address);
+                double latitude = 43.46364889;
+                double longitude = -80.52186743;
+                try {
+                    List<Address> addresses = geocoder.getFromLocationName(textView.getText().toString(), 1);
+                    if(addresses.size() > 0) {
+                        latitude= addresses.get(0).getLatitude();
+                        longitude= addresses.get(0).getLongitude();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                int decidedVehicle = 2;
+                if(carButton.isChecked())
+                    decidedVehicle = 1;
+                else if(bicycleButton.isChecked())
+                    decidedVehicle = 2;
+                else if(motorcycleButton.isChecked())
+                    decidedVehicle = 3;
+
+                Log.d(DEBUGTAG, "Vehicle: " + decidedVehicle);
+                Intent i = new Intent(BrowseSearchActivity.this, MainActivity.class);
+                i.putExtra("searchType", 1);
+                i.putExtra("vehicle", decidedVehicle);
+                i.putExtra("longitude", longitude);
+                i.putExtra("latitude", latitude);
+                startActivity(i);
+            }
+        });
+
     }
 
     @Override
